ce94df493dd410e4e6ddc6299102c432b768d2d5
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-06-24 10:04:41 +0100

    feat(app.d.ts): adds global type declaration

---
+++ src/app.d.ts
@@ -0,0 +1,20 @@
+import { Request as ExpressRequest } from "express";
+
+declare global {
+  interface Request extends ExpressRequest {
+    user?: {
+      id: string;
+    };
+  }
+
+  type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+    T,
+    Exclude<keyof T, Keys>
+  > &
+    {
+      [K in Keys]-?: Required<Pick<T, K>> &
+        Partial<Record<Exclude<Keys, K>, undefined>>;
+    }[Keys];
+
+  type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U;
+}
