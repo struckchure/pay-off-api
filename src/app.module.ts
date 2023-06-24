7f07d5b649affae4ee9e286e768388aee8ab95e5
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-06-24 10:06:05 +0100

    feat(app.module): updates root module with sub modules dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
+import { RedisModule } from "@nestjs-modules/ioredis";
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import "dotenv/config";
 
-import { PrismaModule } from "prisma/prisma.module";
+import { AuthModule } from "@/modules/auth/auth.module";
+import { PrismaModule } from "@/shared/prisma/prisma.module";
+import { UserModule } from "@/modules/user/user.module";
 
-@Module({ imports: [PrismaModule] })
+@Module({
+  imports: [
+    JwtModule.register({ global: true }),
+    RedisModule.forRoot({
+      config: {
+        host: process.env.REDIS_HOST,
+        password: process.env.REDIS_PASSWORD,
+        port: +process.env.REDIS_PORT,
+        username: process.env.REDIS_USERNAME,
+      },
+    }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+  ],
+})
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 
 import { AppModule } from "@/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix("api/:apiVersion");
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
