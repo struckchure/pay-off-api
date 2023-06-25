c92a1424593a6860b4bdcb2e3b2268c2699beb0f
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-06-25 22:37:09 +0100

    feat(src/main): adds global error filter to app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "@/app.module";
+import { ErrorExceptionFilter } from "@/shared/filters/error.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix("api/:apiVersion");
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ErrorExceptionFilter());
 
   await app.listen(3000);
 }
