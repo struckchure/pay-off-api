e13db094354221b997eded8fc8fe683dcb139d57
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-12 18:08:23 +0100

    feat(shared/constants/env-vars): adds redis environment variables

--- src/shared/constants/env-vars.ts
+++ src/shared/constants/env-vars.ts
@@ -8,3 +8,8 @@ export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
 export const JWT_REFRESH_TOKEN_EXPIRY = process.env.JWT_REFRESH_TOKEN_EXPIRY;
 
 export const APP_PORT = +process.env.APP_PORT;
+
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const REDIS_PORT = +process.env.REDIS_PORT;
+export const REDIS_USERNAME = process.env.REDIS_USERNAME;
