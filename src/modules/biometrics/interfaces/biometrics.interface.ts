3e510edccdf61f11a1d681450d12ee1e6925cbb6
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-12 02:52:36 +0100

    refactor(biometrics.interface): updates interface names

--- src/modules/biometrics/interfaces/biometrics.interface.ts
+++ src/modules/biometrics/interfaces/biometrics.interface.ts
@@ -1,36 +1,44 @@
-import { IUser } from "@/modules/user/interfaces/user.interface";
+import { User } from "@/modules/user/interfaces/user.interface";
 
 export enum BiometricType {
   FINGERPRINT = "FINGERPRINT",
 }
 
-export interface IBiometrics {
+export interface Biometrics {
   id: string;
   createdAt: Date;
   updatedAt: Date;
-  user: IUser;
+  user: User;
   biometricType: BiometricType;
   biometricUrl: string;
 }
 
-export interface IBiometricsList {
+export interface BiometricsListArgs {
   userId?: string;
   biometricType?: BiometricType;
   skip?: number;
   take?: number;
 }
 
-export interface IBiometricsCreate {
+export interface BiometricsCreateArgs {
   userId: string;
   biometricType: BiometricType;
   biometricUrl: string;
 }
 
-export interface IBiometricsGet {
+export interface BiometricsGetArgs {
   id?: string;
   userId?: string;
 }
 
-export type IBiometricsUpdate = Partial<Omit<IBiometricsCreate, "userId">>;
+export type BiometricsUpdateArgs = Partial<
+  Omit<BiometricsCreateArgs, "userId">
+>;
 
-export type IBiometricsDelete = IBiometricsGet;
+export type BiometricsDeleteArgs = BiometricsGetArgs;
+
+export interface IBiometricsVerify {
+  biometricType: BiometricType;
+  target: Express.Multer.File;
+  sample: Express.Multer.File;
+}
