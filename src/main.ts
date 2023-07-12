80044faba2ec7bdf8ea587cd5f531b2f075e6b27
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-12 19:26:46 +0100

    fix(src/main): removes global exception filter

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "@/app.module";
 import { APP_PORT } from "@/shared/constants/env-vars";
-import { ErrorExceptionFilter } from "@/shared/filters/error.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix("api/:apiVersion");
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new ErrorExceptionFilter());
 
   await app.listen(APP_PORT);
 }
