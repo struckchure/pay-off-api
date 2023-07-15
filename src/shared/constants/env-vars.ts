c3562dfabeb6c917ddef99e001feb080447ffd89
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-15 16:38:25 +0100

    feat(env-vars): adds flutterwave env variables

--- src/shared/constants/env-vars.ts
+++ src/shared/constants/env-vars.ts
@@ -13,3 +13,7 @@ export const REDIS_HOST = process.env.REDIS_HOST;
 export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 export const REDIS_PORT = +process.env.REDIS_PORT;
 export const REDIS_USERNAME = process.env.REDIS_USERNAME;
+
+export const FLUTTERWAVE_API_URL = process.env.FLUTTERWAVE_API_URL;
+export const FLUTTERWAVE_PK = process.env.FLUTTERWAVE_PK;
+export const FLUTTERWAVE_SK = process.env.FLUTTERWAVE_SK;
