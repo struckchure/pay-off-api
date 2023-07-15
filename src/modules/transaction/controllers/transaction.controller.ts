52f34d8dde72cb15db9c91cd929072c41576aad9
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-15 12:15:31 +0100

    feat(transaction.controller): adds user type and object owner permission to endpoints

--- src/modules/transaction/controllers/transaction.controller.ts
+++ src/modules/transaction/controllers/transaction.controller.ts
@@ -1,19 +1,64 @@
-import { Controller, Get, Param, ParseUUIDPipe, Query } from "@nestjs/common";
+import {
+  Controller,
+  ForbiddenException,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Query,
+  Req,
+  UseGuards,
+} from "@nestjs/common";
 
+import { AllowedUserTypes } from "@/modules/auth/decorators/user-type.decorator";
+import { AuthGuard } from "@/modules/auth/guards/auth.guard";
+import { UserTypeGuard } from "@/modules/auth/guards/user-type.guard";
 import { TransactionListDTO } from "@/modules/transaction/dto/transaction.dto";
 import { TransactionService } from "@/modules/transaction/services/transaction.service";
+import { UserType } from "@prisma/client";
 
 @Controller("transaction")
+@UseGuards(AuthGuard, UserTypeGuard)
 export class TransactionController {
   constructor(private transactionService: TransactionService) {}
 
   @Get()
-  async transactionList(@Query() transactionListDTO: TransactionListDTO) {
+  @AllowedUserTypes(UserType.ADMIN, UserType.CUSTOMER, UserType.MERCHANT)
+  async transactionList(
+    @Query() transactionListDTO: TransactionListDTO,
+    @Req() request: Request,
+  ) {
+    switch (request.user.userType) {
+      case UserType.ADMIN:
+        transactionListDTO.userId = transactionListDTO.userId || null;
+        break;
+      case UserType.CUSTOMER:
+      case UserType.MERCHANT:
+        transactionListDTO.userId = request.user.id;
+        break;
+    }
+
     return await this.transactionService.transactionList(transactionListDTO);
   }
 
   @Get(":transactionId")
-  async transactionGet(@Param(ParseUUIDPipe) transactionId: string) {
-    return await this.transactionService.transactionGet({ id: transactionId });
+  @AllowedUserTypes(UserType.ADMIN, UserType.CUSTOMER, UserType.MERCHANT)
+  async transactionGet(
+    @Param(ParseUUIDPipe) transactionId: string,
+    @Req() request: Request,
+  ) {
+    // TODO: refactor object owner permission here
+    const transaction = await this.transactionService.transactionGet({
+      id: transactionId,
+    });
+    if (
+      request.user.userType === UserType.ADMIN ||
+      transaction.userId !== request.user.id
+    ) {
+      throw new ForbiddenException(
+        "you are not authorized to access this resource",
+      );
+    }
+
+    return transaction;
   }
 }
--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -3,10 +3,11 @@ import { Module } from "@nestjs/common";
 import { TransactionController } from "@/modules/transaction/controllers/transaction.controller";
 import { TransactionDAO } from "@/modules/transaction/dao/transaction.dao";
 import { TransactionService } from "@/modules/transaction/services/transaction.service";
+import { UserDAO } from "@/modules/user/dao/user.dao";
 import { MediaService } from "@/shared/media/media.service";
 
 @Module({
   controllers: [TransactionController],
-  providers: [MediaService, TransactionDAO, TransactionService],
+  providers: [MediaService, TransactionDAO, TransactionService, UserDAO],
 })
 export class TransactionModule {}
