b8609eba24bd9c6f423bde2dcf3dd1100acfdefc
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-16 14:39:49 +0100

    feat(bank.controller): adds auth guard to bank controller

--- src/modules/wallet/controllers/bank.controller.ts
+++ src/modules/wallet/controllers/bank.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 
+import { AuthGuard } from "@/modules/auth/guards/auth.guard";
 import { BankService } from "@/modules/wallet/services/bank.service";
 
 @Controller("bank")
+@UseGuards(AuthGuard)
 export class BankController {
   constructor(private bankService: BankService) {}
 
